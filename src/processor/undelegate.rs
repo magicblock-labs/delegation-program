0af4c2892eca9b9d2da97ad9df125886d613de49
Gabriele Picco <piccogabriele@gmail.com>
2024-05-02 11:44:21 +0100

    :recycle: Code Refactor

---
+++ src/processor/undelegate.rs
@@ -0,0 +1,68 @@
+use std::mem::size_of;
+
+use solana_program::instruction::{AccountMeta, Instruction};
+use solana_program::program::invoke;
+use solana_program::program_error::ProgramError;
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+    system_program, {self},
+};
+
+use crate::consts::{BUFFER, DELEGATION};
+use crate::loaders::{load_owned_pda, load_program, load_signer, load_uninitialized_pda};
+use crate::state::Delegation;
+use crate::utils::create_pda;
+use crate::utils::{AccountDeserialize, Discriminator};
+
+
+/// Undelegate a delegated Pda
+///
+/// 1. Copy origin to a buffer PDA
+/// 2. Close origin and reopen it with authority set to the delegation program
+/// 3. Copy buffer PDA to the origin PDA and close the Authority Record
+///
+/// Accounts expected: Authority Record, Buffer PDA, Delegated PDA
+pub fn process_undelegate<'a, 'info>(
+    _program_id: &Pubkey,
+    accounts: &'a [AccountInfo<'info>],
+    data: &[u8],
+) -> ProgramResult {
+    // TODO: Implement delegation logic
+
+    Ok(())
+}
+
+const CLOSE_INSTRUCTION_DISCRIMINATOR: [u8; 8] = [98, 165, 201, 177, 108, 65, 206, 96];
+
+fn call_close_pda<'a, 'info>(
+    account_to_close: &'a AccountInfo<'info>,
+    destination_account: &'a AccountInfo<'info>,
+    program_id: &Pubkey, // Anchor program's ID
+) -> ProgramResult {
+    let instruction_data = CLOSE_INSTRUCTION_DISCRIMINATOR.to_vec();
+
+    let close_instruction = Instruction {
+        program_id: *program_id,
+        accounts: vec![
+            AccountMeta {
+                pubkey: *account_to_close.key,
+                is_signer: false,
+                is_writable: true,
+            },
+            AccountMeta {
+                pubkey: *account_to_close.key,
+                is_signer: false,
+                is_writable: true,
+            },
+        ],
+        data: instruction_data,
+    };
+
+    invoke(
+        &close_instruction,
+        &[account_to_close.clone(), destination_account.clone()],
+    )
+}
