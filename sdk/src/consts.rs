708c8a312e5689f6144d544df7b5d4ea9e87c6a0
Gabriele Picco <gabriele@magicblock.gg>
2024-06-27 17:05:52 +0100

    :recycle: Code Refactor

--- sdk/src/consts.rs
+++ sdk/src/consts.rs
@@ -6,3 +6,6 @@ pub const BUFFER: &[u8] = b"buffer";
 
 /// The delegation program ID.
 pub const DELEGATION_PROGRAM_ID: Pubkey = pubkey!("DELeGGvXpWV2fqJUhqcF5ZSYMS4JTLjteaAMARRSaeSh");
+
+/// The magic program ID.
+pub const MAGIC_PROGRAM_ID: Pubkey = pubkey!("Magic11111111111111111111111111111111111111");
