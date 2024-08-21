c13d0b41134c8bd54bc541b3869f006dfa23421f
Gabriele Picco <gabriele@magicblock.gg>
2024-08-20 20:15:42 -0600

    chore: Fix integration tests

--- tests/integration/tests/test-fees.ts
+++ tests/integration/tests/test-fees.ts
@@ -1,17 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import {Program, web3} from "@coral-xyz/anchor";
-import * as beet from "@metaplex-foundation/beet";
 
 const FEES_VAULT = "fees-vault";
+const EPHEMERAL_BALANCE = "ephemeral-balance";
 
 describe("TestFees", () => {
-    const connection = new anchor.web3.Connection(anchor.web3.clusterApiUrl('devnet'));
 
-    const provider = new anchor.AnchorProvider(
-        connection,
-        anchor.Wallet.local(),
-        { preflightCommitment: 'confirmed' }
-    );
+    const provider = anchor.AnchorProvider.env();
     anchor.setProvider(provider);
 
     const delegationProgramIdlPath = "./idls/delegation.json";
@@ -23,7 +18,7 @@ describe("TestFees", () => {
         dlpProgram.programId
     );
 
-    it.only('Initializes the fees vault', async () => {
+    it('Initializes the fees vault', async () => {
 
         // Check if the fee vault is initialized
         let feesVaultAccountInfo = await provider.connection.getAccountInfo(feesVaultPda);
@@ -35,35 +30,56 @@ describe("TestFees", () => {
             tx.lastValidBlockHeight = (await provider.connection.getLatestBlockhash()).lastValidBlockHeight;
             tx.feePayer = provider.wallet.publicKey;
             tx = await provider.wallet.signTransaction(tx);
-            console.log(tx.serializeMessage().toString("base64"));
             const txSign = await provider.sendAndConfirm(tx, [], {skipPreflight: true});
             console.log('Init Fees Vault Tx: ', txSign);
         }
-
-        feesVaultAccountInfo = await provider.connection.getAccountInfo(feesVaultPda);
-        console.log('Fees Vault Data: ', feesVaultAccountInfo.data);
     });
 
-    const initializeFeesVaultStruct = new beet.FixableBeetArgsStruct<
-    {
-        instructionDiscriminator: number[] /* size: 8 */;
-    }
-    >(
-        [
-            ["instructionDiscriminator", beet.uniformFixedSizeArray(beet.u8, 8)],
-        ],
-        "InitializeFeesVaultInstructionArgs"
-    );
+    it('TopUp ephemeral balance', async () => {
+        const ix = createTopUpInstruction(provider.wallet.publicKey, 1000000);
+        let tx = new anchor.web3.Transaction().add(ix);
+        tx.recentBlockhash = (await provider.connection.getLatestBlockhash()).blockhash;
+        tx.lastValidBlockHeight = (await provider.connection.getLatestBlockhash()).lastValidBlockHeight;
+        tx.feePayer = provider.wallet.publicKey;
+        tx = await provider.wallet.signTransaction(tx);
+        const txSign = await provider.sendAndConfirm(tx, [], {skipPreflight: true});
+        console.log('TopUp Tx: ', txSign);
+    });
+
+    // Transaction building functions
 
     function createInitializeFeesVaultInstruction(
         payer: anchor.web3.PublicKey,
         programId = dlpProgram.programId
     ) {
-        const [data] = initializeFeesVaultStruct.serialize({
-            instructionDiscriminator: [6, 0, 0, 0, 0, 0, 0, 0],
-        });
+        const data = Buffer.from([6, 0, 0, 0, 0, 0, 0, 0]);
         const keys = [
             { pubkey: payer, isSigner: true, isWritable: true },
+            { pubkey: feesVaultPda, isSigner: false, isWritable: true },
+            { pubkey: web3.SystemProgram.programId, isSigner: false, isWritable: false },
+        ];
+
+        const ix = new web3.TransactionInstruction({
+            programId,
+            keys,
+            data,
+        });
+        return ix;
+    }
+
+    function createTopUpInstruction(
+        payer: anchor.web3.PublicKey,
+        amount: number,
+        programId = dlpProgram.programId
+    ) {
+        const data = Buffer.from([5, 0, 0, 0, 0, 0, 0, 0].concat(new anchor.BN(amount).toArray("le", 8)));
+        const [ephemeralBalancePda] = anchor.web3.PublicKey.findProgramAddressSync(
+            [Buffer.from(EPHEMERAL_BALANCE), payer.toBuffer()],
+            dlpProgram.programId
+        );
+        const keys = [
+            { pubkey: payer, isSigner: true, isWritable: true },
+            { pubkey: ephemeralBalancePda, isSigner: false, isWritable: true },
             { pubkey: feesVaultPda, isSigner: false, isWritable: true },
             { pubkey: web3.SystemProgram.programId, isSigner: false, isWritable: false },
         ];
