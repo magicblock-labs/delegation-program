c7e962e7346c8c5ada2cf4025e8df15d90e2925d
Vincent Brunet <vincent.brunet.us@gmail.com>
2024-12-16 12:27:47 +0900

    remove-valid-until

--- src/args/delegate.rs
+++ src/args/delegate.rs
@@ -3,7 +3,6 @@ use solana_program::pubkey::Pubkey;
 
 #[derive(Default, Debug, BorshSerialize, BorshDeserialize)]
 pub struct DelegateArgs {
-    pub unused_valid_until: i64, // Keeping for ABI backward compat (should be removed later)
     pub commit_frequency_ms: u32,
     pub seeds: Vec<Vec<u8>>,
     pub validator: Option<Pubkey>,
