52d3194ee7578eb57debc74d65f58b6fef28a2b0
Gabriele Picco <piccogabriele@gmail.com>
2025-02-15 14:37:22 -0700

    chore: simplify fees charging structure (#66)

--- src/processor/utils/pda.rs
+++ src/processor/utils/pda.rs
@@ -117,39 +117,51 @@ pub(crate) fn close_pda<'a, 'info>(
 }
 
 /// Close PDA with fees, distributing the fees to the specified addresses in sequence
-#[inline(always)]
+/// The total fees are calculated as `fee_percentage` of the total lamports in the PDA
+/// Each fee address receives fee_percentage % of the previous fee address's amount
 pub(crate) fn close_pda_with_fees<'a, 'info>(
     target_account: &'a AccountInfo<'info>,
     destination: &'a AccountInfo<'info>,
     fees_addresses: &[&AccountInfo<'info>],
     fee_percentage: u8,
 ) -> ProgramResult {
-    let total_lamports = target_account.lamports();
-    let mut remaining_amount = total_lamports;
-
-    // TODO - simplify fees charging logic
-    for fee_address in fees_addresses.iter() {
-        let fee_amount = (remaining_amount as u128)
-            .checked_mul(fee_percentage as u128)
-            .unwrap()
-            .checked_div(100)
-            .unwrap() as u64;
-
-        let fees_starting_lamports = fee_address.lamports();
-        **fee_address.lamports.borrow_mut() =
-            fees_starting_lamports.checked_add(fee_amount).unwrap();
-
-        remaining_amount = remaining_amount.checked_sub(fee_amount).unwrap();
-
-        if remaining_amount == 0 {
-            break;
-        }
+    if fees_addresses.is_empty() || fee_percentage > 100 {
+        return Err(ProgramError::InvalidArgument);
     }
 
-    let dest_starting_lamports = destination.lamports();
-    **destination.lamports.borrow_mut() = dest_starting_lamports
-        .checked_add(remaining_amount)
-        .unwrap();
+    let init_lamports = target_account.lamports();
+    let total_fee_amount = target_account
+        .lamports()
+        .checked_mul(fee_percentage as u64)
+        .and_then(|v| v.checked_div(100))
+        .ok_or(ProgramError::InsufficientFunds)?;
+
+    let mut fees: Vec<u64> = vec![total_fee_amount; fees_addresses.len()];
+
+    let mut fee_amount = total_fee_amount;
+    for fee in fees.iter_mut().take(fees_addresses.len()).skip(1) {
+        fee_amount = fee_amount
+            .checked_mul(fee_percentage as u64)
+            .and_then(|v| v.checked_div(100))
+            .ok_or(ProgramError::InsufficientFunds)?;
+        *fee = fee_amount;
+    }
+
+    for i in 0..fees.len() - 1 {
+        fees[i] -= fees[i + 1];
+    }
+
+    for (i, &fee_address) in fees_addresses.iter().enumerate() {
+        **fee_address.lamports.borrow_mut() = fee_address
+            .lamports()
+            .checked_add(fees[i])
+            .ok_or(ProgramError::InsufficientFunds)?;
+    }
+
+    **destination.lamports.borrow_mut() = destination
+        .lamports()
+        .checked_add(init_lamports - total_fee_amount)
+        .ok_or(ProgramError::InsufficientFunds)?;
 
     **target_account.lamports.borrow_mut() = 0;
     target_account.assign(&solana_program::system_program::ID);
