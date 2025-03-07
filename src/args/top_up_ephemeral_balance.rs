7f5a48dad5adaad2d5b9fe62d6b19b3df3387e21
Thorsten Lorenz <thlorenz@gmx.de>
2025-03-07 18:09:49 +0700

    fix: typo

--- src/args/top_up_ephemeral_balance.rs
+++ src/args/top_up_ephemeral_balance.rs
@@ -5,6 +5,6 @@ pub struct TopUpEphemeralBalanceArgs {
     /// The amount to add to the ephemeral balance.
     pub amount: u64,
     /// The index of the ephemeral balance account to top up which allows
-    /// on payer to have multiple ephemeral balance accounts.
+    /// one payer to have multiple ephemeral balance accounts.
     pub index: u8,
 }
