8c25b59cdeb8ff1717f3a43918b21bb6f3106a5a
taco-paco <edwinswatpako@gmail.com>
2025-10-17 14:54:21 +0300

    fix: return log for tests

--- src/lib.rs
+++ src/lib.rs
@@ -61,6 +61,18 @@ pub fn fast_process_instruction(
         }
     };
 
+    #[cfg(feature = "no-entrypoint")]
+    if matches!(
+        discriminator,
+        DlpDiscriminator::Delegate
+            | DlpDiscriminator::CommitState
+            | DlpDiscriminator::CommitStateFromBuffer
+            | DlpDiscriminator::Finalize
+            | DlpDiscriminator::Undelegate
+    ) {
+        msg!("Processing instruction: {:?}", discriminator);
+    }
+
     match discriminator {
         DlpDiscriminator::Delegate => Some(processor::fast::process_delegate(
             program_id, accounts, data,
