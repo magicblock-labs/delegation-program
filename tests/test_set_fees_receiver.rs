bd8dd995b1b93e6aa01c5ed596ea9bf603290684
Dodecahedr0x <hexadecifish@gmail.com>
2025-10-29 11:29:24 +0100

    test: helper for common code

--- tests/test_set_fees_receiver.rs
+++ tests/test_set_fees_receiver.rs
@@ -38,55 +38,7 @@ async fn test_set_fees_receiver() {
     // Setup
     let (banks, payer, admin, blockhash) = setup_program_test_env(false).await;
 
-    let fees_vault_pda = fees_vault_pda();
-
-    let fees_receiver = Pubkey::new_unique();
-
-    // Set the fees receiver to a new account
-    let ix = dlp::instruction_builder::set_fees_receiver(admin.pubkey(), fees_receiver);
-    let tx = Transaction::new_signed_with_payer(
-        &[ix],
-        Some(&payer.pubkey()),
-        &[&payer, &admin],
-        blockhash,
-    );
-    let res = banks.process_transaction(tx).await;
-    assert!(res.is_ok());
-
-    // Try claiming to the wrong fees receiver
-    let ix = dlp::instruction_builder::protocol_claim_fees(admin.pubkey(), dlp::ID);
-    let tx = Transaction::new_signed_with_payer(&[ix], Some(&payer.pubkey()), &[&payer], blockhash);
-    let res = banks.process_transaction(tx).await;
-    assert!(res.is_err());
-
-    // Claim to the correct fees receiver
-    let ix = dlp::instruction_builder::protocol_claim_fees(fees_receiver, dlp::ID);
-    let tx = Transaction::new_signed_with_payer(&[ix], Some(&payer.pubkey()), &[&payer], blockhash);
-    let res = banks.process_transaction(tx).await;
-    assert!(res.is_ok());
-
-    // Assert that fees vault now only have the rent exemption amount
-    let min_rent = Rent::default().minimum_balance(FeesVault::default().size_with_discriminator());
-    let fees_vault_account = banks.get_account(fees_vault_pda).await.unwrap();
-    assert!(fees_vault_account.is_some());
-    assert_eq!(fees_vault_account.unwrap().lamports, min_rent);
-
-    // Assert that the fees receiver account now has the fees
-    let fees_receiver_account = banks.get_account(fees_receiver).await.unwrap();
-    assert_eq!(
-        fees_receiver_account.unwrap().lamports,
-        LAMPORTS_PER_SOL - min_rent
-    );
-
-    // Assert that FeesVault deserializes correctly and stores the right fees_receiver
-    let data = banks
-        .get_account(fees_vault_pda)
-        .await
-        .unwrap()
-        .unwrap()
-        .data;
-    let vault = FeesVault::try_from_bytes_with_discriminator(&data).unwrap();
-    assert_eq!(Pubkey::from(vault.fees_receiver), fees_receiver);
+    helper_test_set_fees_receiver(&banks, &payer, &admin, blockhash).await;
 }
 
 #[tokio::test]
@@ -94,55 +46,7 @@ async fn test_set_fees_receiver_migration() {
     // Setup
     let (banks, payer, admin, blockhash) = setup_program_test_env_old_fees_vault(true).await;
 
-    let fees_vault_pda = fees_vault_pda();
-
-    let fees_receiver = Pubkey::new_unique();
-
-    // Set the fees receiver to a new account
-    let ix = dlp::instruction_builder::set_fees_receiver(admin.pubkey(), fees_receiver);
-    let tx = Transaction::new_signed_with_payer(
-        &[ix],
-        Some(&payer.pubkey()),
-        &[&payer, &admin],
-        blockhash,
-    );
-    let res = banks.process_transaction(tx).await;
-    assert!(res.is_ok());
-
-    // Try claiming to the wrong fees receiver
-    let ix = dlp::instruction_builder::protocol_claim_fees(admin.pubkey(), dlp::ID);
-    let tx = Transaction::new_signed_with_payer(&[ix], Some(&payer.pubkey()), &[&payer], blockhash);
-    let res = banks.process_transaction(tx).await;
-    assert!(res.is_err());
-
-    // Claim to the correct fees receiver
-    let ix = dlp::instruction_builder::protocol_claim_fees(fees_receiver, dlp::ID);
-    let tx = Transaction::new_signed_with_payer(&[ix], Some(&payer.pubkey()), &[&payer], blockhash);
-    let res = banks.process_transaction(tx).await;
-    assert!(res.is_ok());
-
-    // Assert that fees vault now only have the rent exemption amount
-    let min_rent = Rent::default().minimum_balance(FeesVault::default().size_with_discriminator());
-    let fees_vault_account = banks.get_account(fees_vault_pda).await.unwrap();
-    assert!(fees_vault_account.is_some());
-    assert_eq!(fees_vault_account.unwrap().lamports, min_rent);
-
-    // Assert that the fees receiver account now has the fees
-    let fees_receiver_account = banks.get_account(fees_receiver).await.unwrap();
-    assert_eq!(
-        fees_receiver_account.unwrap().lamports,
-        LAMPORTS_PER_SOL - min_rent
-    );
-
-    // Assert that FeesVault deserializes correctly and stores the right fees_receiver
-    let data = banks
-        .get_account(fees_vault_pda)
-        .await
-        .unwrap()
-        .unwrap()
-        .data;
-    let vault = FeesVault::try_from_bytes_with_discriminator(&data).unwrap();
-    assert_eq!(Pubkey::from(vault.fees_receiver), fees_receiver);
+    helper_test_set_fees_receiver(&banks, &payer, &admin, blockhash).await;
 }
 
 async fn setup_program_test_env(migrate: bool) -> (BanksClient, Keypair, Keypair, Hash) {
@@ -223,4 +127,61 @@ async fn base_setup_program_test_env(
 
     let (banks, payer, blockhash) = program_test.start().await;
     (banks, payer, admin_keypair, blockhash)
+}
+
+async fn helper_test_set_fees_receiver(
+    banks: &BanksClient,
+    payer: &Keypair,
+    admin: &Keypair,
+    blockhash: Hash,
+) {
+    let fees_vault_pda = fees_vault_pda();
+
+    let fees_receiver = Pubkey::new_unique();
+
+    // Set the fees receiver to a new account
+    let ix = dlp::instruction_builder::set_fees_receiver(admin.pubkey(), fees_receiver);
+    let tx = Transaction::new_signed_with_payer(
+        &[ix],
+        Some(&payer.pubkey()),
+        &[&payer, &admin],
+        blockhash,
+    );
+    let res = banks.process_transaction(tx).await;
+    assert!(res.is_ok());
+
+    // Try claiming to the wrong fees receiver
+    let ix = dlp::instruction_builder::protocol_claim_fees(admin.pubkey(), dlp::ID);
+    let tx = Transaction::new_signed_with_payer(&[ix], Some(&payer.pubkey()), &[&payer], blockhash);
+    let res = banks.process_transaction(tx).await;
+    assert!(res.is_err());
+
+    // Claim to the correct fees receiver
+    let ix = dlp::instruction_builder::protocol_claim_fees(fees_receiver, dlp::ID);
+    let tx = Transaction::new_signed_with_payer(&[ix], Some(&payer.pubkey()), &[&payer], blockhash);
+    let res = banks.process_transaction(tx).await;
+    assert!(res.is_ok());
+
+    // Assert that fees vault now only have the rent exemption amount
+    let min_rent = Rent::default().minimum_balance(FeesVault::default().size_with_discriminator());
+    let fees_vault_account = banks.get_account(fees_vault_pda).await.unwrap();
+    assert!(fees_vault_account.is_some());
+    assert_eq!(fees_vault_account.unwrap().lamports, min_rent);
+
+    // Assert that the fees receiver account now has the fees
+    let fees_receiver_account = banks.get_account(fees_receiver).await.unwrap();
+    assert_eq!(
+        fees_receiver_account.unwrap().lamports,
+        LAMPORTS_PER_SOL - min_rent
+    );
+
+    // Assert that FeesVault deserializes correctly and stores the right fees_receiver
+    let data = banks
+        .get_account(fees_vault_pda)
+        .await
+        .unwrap()
+        .unwrap()
+        .data;
+    let vault = FeesVault::try_from_bytes_with_discriminator(&data).unwrap();
+    assert_eq!(Pubkey::from(vault.fees_receiver), fees_receiver);
 }
