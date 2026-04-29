ccdf031eed3898cb4b03d68384ce1fc6cef73036
Gabriele Picco <piccogabriele@gmail.com>
2026-04-29 16:13:46 -0400

    feat: add squads proposal flow (#177)
    * feat: add squads proposal flow
    
    * chore: simplify config
    
    * chore: fix workflow
    
    * chore: fix workflow
    
    * chore: fix workflow
    
    * chore: fix workflow
    
    * chore: fix workflow
    
    * chore: fix workflow
    
    * chore: fix workflow
    
    * chore: fix workflow
    
    * chore: trigger only on manual dispatch

---
+++ .github/scripts/create-squads-upgrade-proposal.mjs
@@ -0,0 +1,111 @@
+import fs from "node:fs";
+import * as multisig from "@sqds/multisig";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  SYSVAR_CLOCK_PUBKEY,
+  SYSVAR_RENT_PUBKEY,
+  TransactionInstruction,
+  TransactionMessage,
+} from "@solana/web3.js";
+
+const BPF_UPGRADEABLE_LOADER_ID = new PublicKey(
+  "BPFLoaderUpgradeab1e11111111111111111111111",
+);
+
+function env(name) {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`${name} is required`);
+  }
+  return value;
+}
+
+function keypairFromFile(path) {
+  return Keypair.fromSecretKey(
+    Uint8Array.from(JSON.parse(fs.readFileSync(path, "utf8"))),
+  );
+}
+
+async function confirmOrThrow(connection, signature, label) {
+  const result = await connection.confirmTransaction(signature, "confirmed");
+  if (result.value.err) {
+    throw new Error(
+      `${label} failed on-chain (signature ${signature}): ${JSON.stringify(result.value.err)}`,
+    );
+  }
+}
+
+const connection = new Connection(env("MAINNET_RPC_URL"), "confirmed");
+const proposer = keypairFromFile(env("PROPOSER_KEYPAIR_PATH"));
+const multisigPda = new PublicKey(env("SQUADS_MULTISIG_PDA"));
+const vaultIndex = Number(env("SQUADS_VAULT_INDEX"));
+const programId = new PublicKey(env("PROGRAM_ID"));
+const bufferAddress = new PublicKey(env("PROGRAM_BUFFER_ADDRESS"));
+const spillAddress = new PublicKey(env("SPILL_ADDRESS"));
+const programDataAddress = new PublicKey(env("PROGRAMDATA_ADDRESS"));
+const proposalName = env("PROPOSAL_NAME");
+
+const [vaultPda] = multisig.getVaultPda({
+  multisigPda,
+  index: vaultIndex,
+});
+
+const upgradeInstruction = new TransactionInstruction({
+  programId: BPF_UPGRADEABLE_LOADER_ID,
+  data: Buffer.from([3, 0, 0, 0]),
+  keys: [
+    { pubkey: programDataAddress, isWritable: true, isSigner: false },
+    { pubkey: programId, isWritable: true, isSigner: false },
+    { pubkey: bufferAddress, isWritable: true, isSigner: false },
+    { pubkey: spillAddress, isWritable: true, isSigner: false },
+    { pubkey: SYSVAR_RENT_PUBKEY, isWritable: false, isSigner: false },
+    { pubkey: SYSVAR_CLOCK_PUBKEY, isWritable: false, isSigner: false },
+    { pubkey: vaultPda, isWritable: false, isSigner: true },
+  ],
+});
+
+const multisigInfo =
+  await multisig.accounts.Multisig.fromAccountAddress(
+    connection,
+    multisigPda,
+  );
+const transactionIndex = BigInt(Number(multisigInfo.transactionIndex) + 1);
+const blockhash = (await connection.getLatestBlockhash()).blockhash;
+const transactionMessage = new TransactionMessage({
+  payerKey: vaultPda,
+  recentBlockhash: blockhash,
+  instructions: [upgradeInstruction],
+});
+
+const vaultTransactionSignature = await multisig.rpc.vaultTransactionCreate({
+  connection,
+  feePayer: proposer,
+  multisigPda,
+  transactionIndex,
+  creator: proposer.publicKey,
+  vaultIndex,
+  ephemeralSigners: 0,
+  transactionMessage,
+  memo: proposalName,
+});
+await confirmOrThrow(connection, vaultTransactionSignature, "vaultTransactionCreate");
+
+const proposalSignature = await multisig.rpc.proposalCreate({
+  connection,
+  feePayer: proposer,
+  multisigPda,
+  transactionIndex,
+  creator: proposer,
+});
+await confirmOrThrow(connection, proposalSignature, "proposalCreate");
+
+fs.appendFileSync(
+  env("GITHUB_ENV"),
+  `SQUADS_TRANSACTION_INDEX=${transactionIndex.toString()}\n`,
+);
+
+console.log(`Squads vault transaction signature: ${vaultTransactionSignature}`);
+console.log(`Squads proposal signature: ${proposalSignature}`);
+console.log(`Squads transaction index: ${transactionIndex.toString()}`);
