8b56c60064cf12e78ce021b2b5f073eca8feb402
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2026-05-06 00:19:15 +0530

    uncomment some mods

--- dlp-api/src/lib.rs
+++ dlp-api/src/lib.rs
@@ -4,43 +4,43 @@ use solana_program::declare_id;
 
 pub mod compat;
 
-// pub mod account_size_class;
+pub mod account_size_class;
 pub mod args;
 pub mod compact;
 pub mod consts;
 
-//#[cfg(feature = "diff")]
-//pub mod diff;
-//
-//pub mod discriminator;
+#[cfg(feature = "diff")]
+pub mod diff;
+
+pub mod discriminator;
 pub mod error;
 pub mod pda;
 pub mod pod_view;
 pub mod requires;
 //pub mod state;
-//
-//pub use account_size_class::*;
-//
+
+pub use account_size_class::*;
+
 //#[cfg(feature = "cpi")]
 //pub mod cpi;
-//
+
 //#[cfg(feature = "instruction")]
 //pub mod instruction_builder;
-//
-//#[cfg(feature = "encryption")]
-//pub mod decrypt;
-//
+
+#[cfg(feature = "encryption")]
+pub mod decrypt;
+
 //#[cfg(feature = "encryption")]
 //pub mod encrypt;
-//
-//#[cfg(feature = "encryption")]
-//pub mod encryption;
-//
-//#[cfg(feature = "encryption")]
-//pub use decrypt::*;
-//
+
+#[cfg(feature = "encryption")]
+pub mod encryption;
+
+#[cfg(feature = "encryption")]
+pub use decrypt::*;
+
 declare_id!("DELeGGvXpWV2fqJUhqcF5ZSYMS4JTLjteaAMARRSaeSh");
-//
+
 pub mod fast {
     pinocchio::address::declare_id!(
         "DELeGGvXpWV2fqJUhqcF5ZSYMS4JTLjteaAMARRSaeSh"
