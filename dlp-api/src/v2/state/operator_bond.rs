052a1cc57854201af606e0cd08a07401e0e0ae2b
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2026-08-24 03:47:41 +0530

    Use layout views for RegisterOperator

--- dlp-api/src/v2/state/operator_bond.rs
+++ dlp-api/src/v2/state/operator_bond.rs
@@ -1,9 +1,6 @@
-use wheels::{
-    fixed_offset_layout,
-    layout::{Decodable, Encodable},
-};
+use wheels::fixed_offset_layout;
 
-use crate::{compat::Pubkey, solana_program::program_error::ProgramError};
+use crate::compat::Pubkey;
 
 pub const OPERATOR_STATUS_ACTIVE: u8 = 1;
 pub const OPERATOR_STATUS_EXITING: u8 = 2;
@@ -13,46 +10,25 @@ pub const OPERATOR_STATUS_JAILED: u8 = 4;
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[fixed_offset_layout(buffer_offset = 0)]
 pub struct OperatorBond {
+    /// Account type marker.
+    pub discriminator: [u8; 8],
+
+    /// Operator identity allowed to post commitments through this bond.
     pub operator_identity: Pubkey,
+
+    /// Slashable operator stake held in this account.
     pub stake_lamports: u64,
+
+    /// Stake reserved by active commitments.
     pub locked_lamports: u64,
+
+    /// Current operator lifecycle state.
     pub status: u8,
+
+    /// Slot when withdrawal was requested, if the operator is exiting.
     pub withdraw_requested_slot: Option<u64>,
 }
 
 impl OperatorBond {
     pub const DISCRIMINATOR: [u8; 8] = *b"v2opbond";
-    pub const SPACE: usize = 8 + Self::DATA_LEN;
-
-    pub fn to_bytes_with_discriminator(
-        &self,
-        data: &mut [u8],
-    ) -> Result<(), ProgramError> {
-        let payload = super::utils::payload_with_discriminator_mut(
-            &Self::DISCRIMINATOR,
-            data,
-        )?;
-        self.encode_to(payload)
-            .map_err(super::utils::layout_error_to_program_error)?;
-        Ok(())
-    }
-
-    pub fn try_from_bytes_with_discriminator(
-        data: &[u8],
-    ) -> Result<Self, ProgramError> {
-        let payload = super::utils::payload_with_discriminator(
-            &Self::DISCRIMINATOR,
-            data,
-        )?;
-        let view = <Self as Decodable>::decode(payload)
-            .map_err(super::utils::layout_error_to_program_error)?;
-
-        Ok(Self {
-            operator_identity: *view.operator_identity(),
-            stake_lamports: view.stake_lamports(),
-            locked_lamports: view.locked_lamports(),
-            status: view.status(),
-            withdraw_requested_slot: view.withdraw_requested_slot(),
-        })
-    }
 }
--- src/v2/processor/bootstrap/register_operator.rs
+++ src/v2/processor/bootstrap/register_operator.rs
@@ -6,20 +6,23 @@ use dlp_api::{
         OPERATOR_STATUS_ACTIVE,
     },
 };
-use solana_sdk_ids::system_program;
-use solana_system_interface::instruction as system_instruction;
+use pinocchio::{
+    address::Address,
+    cpi::{Seed, Signer},
+    error::ProgramError,
+    AccountView, ProgramResult,
+};
+use pinocchio_system::instructions as system;
+use wheels::{
+    layout::{Decodable, Encodable},
+    require_eq_keys, require_ge, require_n_accounts, require_ne,
+    require_signer,
+};
 
 use crate::{
-    processor::utils::{
-        loaders::{
-            load_initialized_pda, load_program, load_signer,
-            load_uninitialized_pda,
-        },
-        pda::create_pda,
-    },
-    solana_program::{
-        account_info::AccountInfo, entrypoint::ProgramResult, program::invoke,
-        program_error::ProgramError, pubkey::Pubkey,
+    processor::fast::utils::pda::create_pda,
+    requires::{
+        require_initialized_pda, require_uninitialized_pda, StandardCtx,
     },
 };
 
@@ -30,90 +33,91 @@ use crate::{
 /// 1: `[signer]`           protocol authority that admits the operator
 /// 2: `[writable]`         OperatorBond PDA
 /// 3: `[]`                 ProtocolConfig PDA
-/// 4: `[]`                 system program
+/// 4: `[]`                 system program, required by system CPI
+#[inline(never)]
 pub fn process_register_operator(
-    _program_id: &Pubkey,
-    accounts: &[AccountInfo],
+    accounts: &[AccountView],
     data: &[u8],
 ) -> ProgramResult {
-    let args = RegisterOperatorArgs::try_from_bytes(data)?;
+    let [
+        operator, // force multi-line
+        authority,
+        operator_bond,
+        protocol_config,
+        _system_program,
+    ] = require_n_accounts!(accounts, 5);
 
-    let [operator, authority, operator_bond, protocol_config, system_program] =
-        accounts
-    else {
-        return Err(ProgramError::NotEnoughAccountKeys);
-    };
+    let args = RegisterOperatorArgs::decode(data)?;
 
-    load_signer(operator, "operator")?;
-    load_signer(authority, "authority")?;
-    load_program(system_program, system_program::id(), "system program")?;
+    require_signer!(operator);
+    require_signer!(authority);
 
-    load_initialized_pda(
+    require_initialized_pda(
         protocol_config,
         &[PROTOCOL_CONFIG_SEED],
-        &crate::id(),
+        &crate::fast::ID,
         false,
         "protocol config",
     )?;
-
-    let protocol_config_data = protocol_config.try_borrow_data()?;
+    let protocol_config_data = protocol_config.try_borrow()?;
     let protocol_config_state =
-        ProtocolConfig::try_from_bytes_with_discriminator(
-            protocol_config_data.as_ref(),
-        )?;
-
-    if protocol_config_state.authority != *authority.key {
-        return Err(DlpError::InvalidAuthority.into());
+        ProtocolConfig::decode(protocol_config_data.as_ref())?;
+    if protocol_config_state.discriminator() != ProtocolConfig::DISCRIMINATOR {
+        return Err(ProgramError::InvalidAccountData);
     }
+    require_eq_keys!(
+        &Address::from(protocol_config_state.authority().to_bytes()),
+        authority.address(),
+        DlpError::InvalidAuthority
+    );
+    require_ne!(
+        args.amount_lamports(),
+        0,
+        ProgramError::InvalidInstructionData
+    );
+    require_ge!(
+        args.amount_lamports(),
+        protocol_config_state.min_operator_bond(),
+        ProgramError::InvalidInstructionData
+    );
+    drop(protocol_config_data);
 
-    if *operator.key == Pubkey::default()
-        || args.amount_lamports < protocol_config_state.min_operator_bond
-    {
-        return Err(ProgramError::InvalidInstructionData);
-    }
-
-    let operator_bond_bump = load_uninitialized_pda(
+    let operator_bond_bump = require_uninitialized_pda(
         operator_bond,
-        &[OPERATOR_BOND_SEED, operator.key.as_ref()],
-        &crate::id(),
+        &[OPERATOR_BOND_SEED, operator.address().as_ref()],
+        &crate::fast::ID,
         true,
-        "operator bond",
+        StandardCtx::new("operator bond"),
     )?;
 
     create_pda(
         operator_bond,
-        &crate::id(),
-        OperatorBond::SPACE,
-        &[OPERATOR_BOND_SEED, operator.key.as_ref()],
-        operator_bond_bump,
-        system_program,
+        &crate::fast::ID,
+        OperatorBond::DATA_LEN,
+        &[Signer::from(&[
+            Seed::from(OPERATOR_BOND_SEED),
+            Seed::from(operator.address().as_ref()),
+            Seed::from(&[operator_bond_bump]),
+        ])],
         operator,
     )?;
 
-    invoke(
-        &system_instruction::transfer(
-            operator.key,
-            operator_bond.key,
-            args.amount_lamports,
-        ),
-        &[
-            operator.clone(),
-            operator_bond.clone(),
-            system_program.clone(),
-        ],
-    )?;
+    system::Transfer {
+        from: operator,
+        to: operator_bond,
+        lamports: args.amount_lamports(),
+    }
+    .invoke()?;
 
-    let operator_bond_state = OperatorBond {
-        operator_identity: *operator.key,
-        stake_lamports: args.amount_lamports,
+    OperatorBond {
+        discriminator: OperatorBond::DISCRIMINATOR,
+        operator_identity: operator.address().to_bytes().into(),
+        stake_lamports: args.amount_lamports(),
         locked_lamports: 0,
         status: OPERATOR_STATUS_ACTIVE,
         withdraw_requested_slot: None,
-    };
-
-    let mut operator_bond_data = operator_bond.try_borrow_mut_data()?;
-    operator_bond_state
-        .to_bytes_with_discriminator(operator_bond_data.as_mut())?;
+    }
+    .encode_to(operator_bond.try_borrow_mut()?.as_mut())?;
 
     Ok(())
 }
